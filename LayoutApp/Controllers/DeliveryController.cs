b94b9f534a219abbbc2a1d7282211bcaffc4c6e3
Mohammad Abu Snober <Mohammad Abu Snober@DESKTOP-RCU1R3G>
2024-03-25 13:57:50 +0300

    Add project files.

---
+++ LayoutApp/Controllers/DeliveryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutApp.Controllers
+{
+    public class DeliveryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Grocery()
+        {
+            return View();
+        }
+
+        public IActionResult Pharmacy()
+        {
+            return View();
+        }
+
+        public IActionResult Restaurant()
+        {
+            return View();
+        }
+        public IActionResult Order()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ LayoutApp/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LayoutApp.Models
+{
+    public class Order
+    {
+    }
+
+    public enum Location
+    {
+        Jubaiha,
+        Khalda,
+        Jamea,
+        [Display(Name="Amman North") ] ANoooo
+
+    }
+}
