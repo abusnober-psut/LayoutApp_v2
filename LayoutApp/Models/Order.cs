7852767bd5568da67b24231a1c0cf562b85c1cac
Dr. Mohammad Abu Snober <115994136+abusnober-psut@users.noreply.github.com>
2024-04-18 22:50:04 +0300

    Add files via upload

--- LayoutApp/Models/Order.cs
+++ LayoutApp/Models/Order.cs
@@ -3,6 +3,32 @@ namespace LayoutApp.Models
 {
     public class Order
     {
+        [Required(ErrorMessage = "Order Id cannot be Empty")]
+        [RegularExpression("[0-9]{4}", ErrorMessage = "Order Id should be 4 digits length")]
+        public int OrderId { get; set; }
+
+        [Required]
+        [RegularExpression(@"(AMM|ZAQ|IRB)\d{3}", ErrorMessage = "Order Key have to be started with zone code")]
+        public string OrderKey { get; set; }
+
+        [Required]
+        [RegularExpression(@"00962 (78|77|79) \d{4} \d{3}")]
+        public string Phone { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Range(0.5, 100, ErrorMessage ="Price cannot be negative value")]
+        public double Price{get; set;}
+
+        [Length(0, 100)]
+        public string Destination { get; set; }
+
     }
 
     public enum Location
@@ -10,7 +36,7 @@ namespace LayoutApp.Models
         Jubaiha,
         Khalda,
         Jamea,
-        [Display(Name="Amman North") ] ANoooo
+        [Display(Name="Amman North") ] AN
 
     }
 }
